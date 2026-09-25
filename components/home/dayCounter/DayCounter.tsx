f01fc773ad199f60de35c155ee0099868a5b12fb
Tajrian Tahmin <tajriantahmin@gmail.com>
2026-09-25 22:39:26 +0600

    Re-sync repo with local files

--- components/home/dayCounter/DayCounter.tsx
+++ components/home/dayCounter/DayCounter.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import styles from "./DayCounter.module.css";
+import styles from "@/components/home/dayCounter/DayCounter.module.css";
 
 interface TimeTogether {
     days: number;
